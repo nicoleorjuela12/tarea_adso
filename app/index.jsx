dc5b1f2ae3a48fa3f5fc58d44b2868ed1b157f9c
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2024-08-12 10:32:58 -1200

    parte 3 del video botones

--- app/index.jsx
+++ app/index.jsx
@@ -7,9 +7,9 @@ export default function App(){
 
   return(
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl"> Aora Adso¡</Text>
+      <Text className="text-3xl font-pblack"> Aora Adso¡</Text>
       <StatusBar style='auto'/>
-      <Link href="/profile" style={{color:'blue'}}>Go to Profile</Link>
+      <Link href="/home" style={{color:'blue'}}>Go to Home</Link>
     </View>
 
   )
