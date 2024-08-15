c2c7e258a5e6cc1cf69b6686d209c4282b8a4021
unknown <Aprendiz@BOGDFPCGMS4129.FormacionSena.RED>
2024-08-15 16:59:52 -0500

    parte 3

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { ScrollView } from 'react-native-gesture-handler'
+
+import {images} from '../../constants'
+import { useState } from 'react'
 
 const SignIn= () => {
+
+  const [form, setForm] = useState({
+
+    email:'',
+    password:''
+
+  })
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image source={images.logo}
+          resizeMode='contain' className="w-[115px] h-[35px]"/>
+
+          <Text  className="text-2xl text-white text-semibold mt-10 font-psemibold">Long in to 
+          Aora</Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handChangeText={(e) => setForm ({ ...form, email: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handChangeText={(e) => setForm ({ ...form, password: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+            
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
